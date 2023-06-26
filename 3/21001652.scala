4d2f48e0a851fdc0c1774301b181964e332f6b7c
LNTR <101091694+LNTR@users.noreply.github.com>
2023-06-26 11:15:02 +0530

    Assignment 3 Commited

---
+++ 3/21001652.scala
@@ -0,0 +1,23 @@
+object Q1 extends App{
+    def getReverse(inputString:String):String= if(inputString=="") "" 
+    else inputString.last.toString()+getReverse(inputString.slice(0,inputString.length()-1))
+    println(getReverse("123456789"))
+
+
+    def getStringsListLargerThan5(stringList:List[String])={
+        var stringListLargerThanFive:List[String]=List()
+        stringList.foreach(elem=>{
+            if (isLengthLargerThan5(elem)){
+                stringListLargerThanFive= stringListLargerThanFive :+elem
+            }
+        })
+        stringListLargerThanFive
+    }
+    def isLengthLargerThan5(elem:String)=elem.length()>5;
+
+    println(getStringsListLargerThan5(List("12345678","12","12345","123456789")))
+
+    def getAvarage(num1:Int,num2:Int):Float=(num1+num2).toFloat/2
+    println(getAvarage(5,6))
+
+}
